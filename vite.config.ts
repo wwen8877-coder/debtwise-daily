d4ed1c9ed57156849f4c4e065dfcd33b48f751b1
wwen8877-coder <wwen8877@gmail.com>
2026-04-07 10:09:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 👇 关键！必须改成你的仓库名！
+  // 
   base: '/debtwise-daily/',
   plugins: [react()],
   server: {
